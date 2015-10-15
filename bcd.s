1a31b2957689026190f90863f2800b1321d7ef56
Sandi Krivec <sandi.krivec@hotmail.com>
2015-10-15 12:38:25 +0200

    added comments and prints negative numbers

--- bcd.s
+++ bcd.s
@@ -1,63 +1,77 @@
 .code32
 .data # Initialised data
 	NEWLINE: .asciz "\n"
+	MINUS: .asciz "-"
 .bss #uninitialised data
+	.lcomm COUNTER, 4
 	.lcomm TEMP1, 4
-	.lcomm a,1
-	.lcomm b,1
-	.lcomm c,1
-	.lcomm d,1
-	.lcomm e,1
-	.lcomm f,1
-	.lcomm g,1
-	.lcomm h,1
-	.lcomm i,1
-	.lcomm j,1
-	.lcomm k,1
-	.lcomm l,1
-	.lcomm m,1
-	.lcomm n,1
-	.lcomm o,1
-	.lcomm p,1
-	.lcomm q,1
-	.lcomm r,1
-	.lcomm s,1
-	.lcomm t,1
-	.lcomm u,1
-	.lcomm v,1
-	.lcomm w,1
-	.lcomm x,1
-	.lcomm y,1
-	.lcomm z,1
+	.lcomm a,4
+	.lcomm b,4
+	.lcomm c,4
+	.lcomm d,4
+	.lcomm e,4
+	.lcomm f,4
+	.lcomm g,4
+	.lcomm h,4
+	.lcomm i,4
+	.lcomm j,4
+	.lcomm k,4
+	.lcomm l,4
+	.lcomm m,4
+	.lcomm n,4
+	.lcomm o,4
+	.lcomm p,4
+	.lcomm q,4
+	.lcomm r,4
+	.lcomm s,4
+	.lcomm t,4
+	.lcomm u,4
+	.lcomm v,4
+	.lcomm w,4
+	.lcomm x,4
+	.lcomm y,4
+	.lcomm z,4
 .text
 .globl _start # _start on linux
 	.type print, @function
 	.type printNumber, @function
+	.type gcd, @function
 	printNumber:
+		movl $0, COUNTER
+		cmpl $0, %eax #is it negative number
+		jge loop #if eax is greater or equal zero then go to loop
+		neg %eax #change last bit to 0
+		push %eax #save eax
+		push $1
+		push $MINUS
+		call print
+		addl $8, %esp #restore stack pointer for 2 arguments
+		pop %eax #get back value
 		loop:
-			movl $0, %edx
+			movl $0, %edx #MSByte
 			movl $10, %ebx
-			divl %ebx
-			addl $48, %edx
+			idiv %ebx #eax=edx:eax/ebx; # edx=edx:eax MOD ebx 13/10=1, 13 mod 10 = 3 => push 3 on stack, 1 is in eax
+			addl $48, %edx #add 48, check ASCII, zero is 48
 			pushl %edx
-			incl %esi
+			incl COUNTER #increment by one
 			cmpl $0, %eax
-			jz   next
+			jz   printN #if zero then we have to print otherwise loop
 			jmp loop
 
-		next:
-			cmpl $0, %esi
+		printN:
+			cmpl $0, COUNTER
 			jz   return
-			decl %esi
+			decl COUNTER
 			movl $4, %eax
-			movl %esp, %ecx
 			movl $1, %ebx
-			movl $1, %edx
+			movl $1, %edx #how many bytes
+			movl %esp, %ecx #ecx should contain pointer to ascii
 			int  $0x80
-			addl $4, %esp
-			jmp  next 
+			addl $4, %esp #decrease stack size, get next number on stack
+			jmp  printN
 		return:
 			ret
+
 	print:
 		movl $4, %eax
 		movl $1, %ebx
@@ -65,16 +79,45 @@
 		movl 8(%esp), %edx #point to first argument which is on the stack
 		int $0x80 # call kernel
 		ret #change eip to start next instruction
+	gcd:
+		movl 8(%esp), %eax #get first parameter
+		movl 4(%esp), %edx #get second parameter
+		start:
+			cmp %eax, %edx
+			je	end
+			cmp %edx, %eax
+			jg	middle #if eax > edx then jmp
+			sub %eax, %edx #assign edx
+			jmp	start
+		middle:
+			sub %edx, %eax #assign eax
+			jmp	start
+		end:
+			ret
 _start:
-	push	$5
-	push	$4
+	push	$1
+	push	$2
+	pop TEMP1
+	pop %eax
+	sub TEMP1, %eax
+	push %eax
+
+	pop	n
+	push	n
+	pop %eax
+	call printNumber
+	push $1
+	push $NEWLINE
+	call print
+	push	n
+	push	$2
 	pop TEMP1
 	pop %eax
 	add TEMP1, %eax
 	push %eax
 
-	pop	a
-	push	a
+	pop	n
+	push	n
 	pop %eax
 	call printNumber
 	push $1
--- lexyacc-code/calc3i.c
+++ lexyacc-code/calc3i.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include "calc3.h"
 #include "y.tab.h"
 
 static int lbl;
 static int i=0;
+char jmpType[4];
 
 // here we need replace push, pop,... with i386 assembler instructions
 //You will need to allocate space for the symbol table and emit some
@@ -13,18 +15,19 @@ int ex(nodeType *p) {
     int lbl1, lbl2;
     //print assembly header
     if(i++==0) {
-    	char *initialisedData = ".data # Initialised data\n\tNEWLINE: .asciz \"\\n\"";
+    	char *initialisedData = ".data # Initialised data\n\tNEWLINE: .asciz \"\\n\"\n\tMINUS: .asciz \"-\"";
     	printf(".code32\n%s\n.bss #uninitialised data\n",initialisedData);
-    	puts("\t.lcomm TEMP1, 4");
+    	puts("\t.lcomm COUNTER, 4\n\t.lcomm TEMP1, 4");
     	//print variables
     	char c;
 		  for(c='a'; c<='z'; ++c)
-		     printf("\t.lcomm %c,1\n",c);
+		     printf("\t.lcomm %c,4\n",c);
 			printf(".text\n.globl _start # _start on linux\n");	
-			char *types="\t.type print, @function\n\t.type printNumber, @function";
-			char* printNumber="\n\tprintNumber:\n\t\tloop:\n\t\t\tmovl $0, %edx\n\t\t\tmovl $10, %ebx\n\t\t\tdivl %ebx\n\t\t\taddl $48, %edx\n\t\t\tpushl %edx\n\t\t\tincl %esi\n\t\t\tcmpl $0, %eax\n\t\t\tjz   next\n\t\t\tjmp loop\n\n\t\tnext:\n\t\t\tcmpl $0, %esi\n\t\t\tjz   return\n\t\t\tdecl %esi\n\t\t\tmovl $4, %eax\n\t\t\tmovl %esp, %ecx\n\t\t\tmovl $1, %ebx\n\t\t\tmovl $1, %edx\n\t\t\tint  $0x80\n\t\t\taddl $4, %esp\n\t\t\tjmp  next \n\t\treturn:\n\t\t\tret";
-			char* print="\n\tprint:\n\t\tmovl $4, %eax\n\t\tmovl $1, %ebx\n\t\tmovl 4(%esp), %ecx #point to first argument which is on the stack\n\t\tmovl 8(%esp), %edx #point to first argument which is on the stack\n\t\tint $0x80 # call kernel\n\t\tret #change eip to start next instruction";
-			printf("%s%s%s\n",types,printNumber, print);
+			char *types="\t.type print, @function\n\t.type printNumber, @function\n\t.type gcd, @function\n";
+			char* printNumber="\tprintNumber:\n\t\tmovl $0, COUNTER\n\t\tcmpl $0, %eax #is it negative number\n\t\tjge loop #if eax is greater or equal zero then go to loop\n\t\tneg %eax #change last bit to 0\n\t\tpush %eax #save eax\n\t\tpush $1\n\t\tpush $MINUS\n\t\tcall print\n\t\taddl $8, %esp #restore stack pointer for 2 arguments\n\t\tpop %eax #get back value\n\t\tloop:\n\t\t\tmovl $0, %edx #MSByte\n\t\t\tmovl $10, %ebx\n\t\t\tidiv %ebx #eax=edx:eax/ebx; # edx=edx:eax MOD ebx 13/10=1, 13 mod 10 = 3 => push 3 on stack, 1 is in eax\n\t\t\taddl $48, %edx #add 48, check ASCII, zero is 48\n\t\t\tpushl %edx\n\t\t\tincl COUNTER #increment by one\n\t\t\tcmpl $0, %eax\n\t\t\tjz   printN #if zero then we have to print otherwise loop\n\t\t\tjmp loop\n\n\t\tprintN:\n\t\t\tcmpl $0, COUNTER\n\t\t\tjz   return\n\t\t\tdecl COUNTER\n\t\t\tmovl $4, %eax\n\t\t\tmovl $1, %ebx\n\t\t\tmovl $1, %edx #how many bytes\n\t\t\tmovl %esp, %ecx #ecx should contain pointer to ascii\n\t\t\tint  $0x80\n\t\t\taddl $4, %esp #decrease stack size, get next number on stack\n\t\t\tjmp  printN\n\t\treturn:\n\t\t\tret\n";
+			char* print="\n\tprint:\n\t\tmovl $4, %eax\n\t\tmovl $1, %ebx\n\t\tmovl 4(%esp), %ecx #point to first argument which is on the stack\n\t\tmovl 8(%esp), %edx #point to first argument which is on the stack\n\t\tint $0x80 # call kernel\n\t\tret #change eip to start next instruction\n";
+			char* gcd = "\tgcd:\n\t\tmovl 8(%esp), %eax #get first parameter\n\t\tmovl 4(%esp), %edx #get second parameter\n\t\tstart:\n\t\t\tcmp %eax, %edx\n\t\t\tje\tend\n\t\t\tcmp %edx, %eax\n\t\t\tjg\tmiddle #if eax > edx then jmp\n\t\t\tsub %eax, %edx #assign edx\n\t\t\tjmp\tstart\n\t\tmiddle:\n\t\t\tsub %edx, %eax #assign eax\n\t\t\tjmp\tstart\n\t\tend:\n\t\t\tret\n";
+			printf("%s%s%s%s",types,printNumber, print,gcd);
 			printf("_start:\n");
 		}
     if (!p) return 0;
@@ -40,7 +43,7 @@ int ex(nodeType *p) {
         case WHILE:
             printf("L%03d:\n", lbl1 = lbl++);
             ex(p->opr.op[0]);
-            printf("\tjz\tL%03d\n", lbl2 = lbl++);
+            printf("\t%s\tL%03d\n",jmpType, lbl2 = lbl++);
             ex(p->opr.op[1]);
             printf("\tjmp\tL%03d\n", lbl1);
             printf("L%03d:\n", lbl2);
@@ -49,7 +52,7 @@ int ex(nodeType *p) {
             ex(p->opr.op[0]);
             if (p->opr.nops > 2) {
                 /* if else */
-                printf("\tjz\tL%03d\n", lbl1 = lbl++);
+                printf("\t%s\tL%03d\n",jmpType, lbl1 = lbl++);
                 ex(p->opr.op[1]);
                 printf("\tjmp\tL%03d\n", lbl2 = lbl++);
                 printf("L%03d:\n", lbl1);
@@ -57,7 +60,7 @@ int ex(nodeType *p) {
                 printf("L%03d:\n", lbl2);
             } else {
                 /* if */
-                printf("\tjz\tL%03d\n", lbl1 = lbl++);
+                printf("\t%s\tL%03d\n",jmpType, lbl1 = lbl++);
                 ex(p->opr.op[1]);
                 printf("L%03d:\n", lbl1);
             }
@@ -75,7 +78,7 @@ int ex(nodeType *p) {
             break;
         case UMINUS:    
             ex(p->opr.op[0]);
-            printf("\tneg\n");
+            puts("\tpop %eax\n\tneg %eax\n\tpush %eax");
             break;
 	case FACT:
   	    ex(p->opr.op[0]);
@@ -89,18 +92,18 @@ int ex(nodeType *p) {
             ex(p->opr.op[0]);
             ex(p->opr.op[1]);
             switch(p->opr.oper) {
-	    case GCD:   printf("\tgcd\n"); break;
+	    case GCD:   puts("\tcall gcd\n\tpush %eax"); break;
 	    			//get from the stack and save into TEMP1 then into eax and add TEMP1 to eax, push on the stack
             case '+':   puts("\tpop TEMP1\n\tpop %eax\n\tadd TEMP1, %eax\n\tpush %eax\n"); break;
-            case '-':   printf("\tsub\n"); break; 
-            case '*':   printf("\tmul\n"); break;
-            case '/':   printf("\tdiv\n"); break;
-            case '<':   printf("\tcompLT\n"); break;
-            case '>':   printf("\tcompGT\n"); break;
-            case GE:    printf("\tcompGE\n"); break;
-            case LE:    printf("\tcompLE\n"); break;
-            case NE:    printf("\tcompNE\n"); break;
-            case EQ:    printf("\tcompEQ\n"); break;
+            case '-':   puts("\tpop TEMP1\n\tpop %eax\n\tsub TEMP1, %eax\n\tpush %eax\n"); break;
+            case '*':   puts("\tpop TEMP1\n\tpop %eax\n\timul TEMP1, %eax\n\tpush %eax\n"); break;
+            case '/':   puts("\tpop TEMP1\n\tpop %eax\n\tidiv TEMP1, %eax\n\tpush %eax\n"); break;
+            case '<':   puts("\tpop TEMP1\n\tpop %eax\n\tcmp %eax, TEMP1"); strcpy (jmpType,"jl\0"); break;
+            case '>':   puts("\tpop TEMP1\n\tpop %eax\n\tcmp %eax, TEMP1"); strcpy (jmpType,"jg\0"); break;
+            case GE:    puts("\tpop TEMP1\n\tpop %eax\n\tcmp %eax, TEMP1"); strcpy (jmpType,"jge\0"); break;
+            case LE:    puts("\tpop TEMP1\n\tpop %eax\n\tcmp %eax, TEMP1"); strcpy (jmpType,"jle\0"); break;
+            case NE:    puts("\tpop TEMP1\n\tpop %eax\n\tcmp %eax, TEMP1"); strcpy (jmpType,"je\0"); break;
+            case EQ:    puts("\tpop TEMP1\n\tpop %eax\n\tcmp %eax, TEMP1"); strcpy (jmpType,"jne\0"); break;
             }
         }
     }
