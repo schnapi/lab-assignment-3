8a21314f5af9a5429ca62f7821e8d60c9b604913
Romain FONTAINE <romain@ns3008186.ip-5-135-184.eu>
2015-10-21 11:28:09 +0200

    Added support for '0! = 1'

--- src/fact.s
+++ src/fact.s
@@ -3,9 +3,14 @@
 	.type fact, @function
 	fact:
 		movl	4(%esp), %ecx	# get the parameter
+		cmp	$0, %ecx	# if param <= 0
+		jle	lezero		# return 1
 		movl	$1, %eax	# eax = 1
 	l1:
 		mull	%ecx		# eax *= ecx
 		loop 	l1		# Decrement ecx, go to l1 while ecx != 0
+		ret
+	lezero:
+		movl	$1, %eax	# eax = 1 & return
 		ret
 
